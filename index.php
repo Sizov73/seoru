95dfa5b938005216cbaa720a2dcc84bb30877c65
Sizov73 <33544685+Sizov73@users.noreply.github.com>
2019-07-18 11:59:58 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<title>Пример использования Яндекс.XML: парсер сниппета поисковой системы Яндекс</title> 
+	</head>
+	<body>
+		<form method="post" action="yandex-result.php">
+			<p>
+				<b>Введите поисковую фразу:</b><br/>
+				<input name="key" type="text" size="20">
+			</p>
+			<p>
+				<b>Введите регион:</b><br/>
+				<select  name="city">
+					<option value="193">Воронеж</option>
+					<option value="213">Москва</option>
+				</select>
+			</p>
+			<p>
+				<input type="submit" value="Отправить">
+			</p>
+		</form>
+	</body>
+</html>
---
+++ yandex-result.php
@@ -0,0 +1,17 @@
+﻿<?php
+	//Подключаем библиотеку парсера
+	include "simple_html_dom.php";
+	//Выводим введенный нами поисковой запрос
+	echo('<b>Результат по запросу:</b> '.$_POST['key'].'<br/><br/>');
+	$url = "https://yandex.ru/search/xml?user=sizov73ul&key=03.184190486:147b5f2696a2f6c1c2650a24e9c7e7b4&query=".urlencode($_POST['key']).'&lr='.$_POST['city'];
+	//Парсим страницу по сформированному запросу:
+	echo $url;
+	$html = file_get_html($url);
+	echo $html;
+	//Выводим результат в текстовое поле
+	echo('<textarea name="comment" cols="40" rows="3">'.$html.'</textarea>');
+	
+	//Вывод всех слов из подстветки сниппета
+	foreach($html->find('a', 0) as $element) {
+		echo $element->innertext.' / ';
+	}
